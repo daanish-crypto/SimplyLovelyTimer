1d1fe34e3ebfef8417a4d40afd2da78cbadac278
daanish-crypto <daanishverma11@gmail.com>
2026-05-21 21:58:55 +0530

    tried to add a menu bar for timer customizing and eventually just added two buttons below the timer.

--- guimain.py
+++ guimain.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import Menu
 from PIL import Image
 import pygame
 
@@ -8,8 +9,38 @@ ctk.set_default_color_theme("dark-blue")
 
 app = ctk.CTk()
 app.configure(fg_color="#0C151E")
-app.geometry("747x490")
+app.geometry("850x460")
 app.title("SimplyLovelyTimer")
+
+menu_bar = Menu(app)
+app.config(menu=menu_bar)
+'''
+setting_menu = Menu(
+    menu_bar,
+
+    tearoff=0,
+
+    bg="#151515",
+    fg="white",
+
+    activebackground="#891212",
+    activeforeground="white"
+)
+
+menu_bar.add_cascade(
+    label="Settings",
+    menu=setting_menu
+)
+
+setting_menu.add_command(
+    label="Timers"
+)
+
+setting_menu.add_command(
+    label="Exit",
+    command=app.quit
+)
+'''
 # -------------------------------------
 
 # assets -------------------------------
@@ -32,8 +63,8 @@ running = False
 session_type = "Study"
 timer_id = None
 car_x = 20
-car_y = 190
-button_frame_y = 150
+car_y = 160
+button_frame_y = 140
 # ---------------------------------------
 
 # clock ---------------------------------
@@ -75,32 +106,31 @@ def animate_car():
     global car_x
     global running
 
-    if car_x < 700:
+    if car_x < 850:
         car_x = car_x + 7
         car_label.place(x=car_x,y=car_y)
         app.after(16, animate_car)
     else:
         car_label.place_forget()
-        animate_buttons()
-        
+        animate_buttons() 
 
 def animate_buttons():
     global running
     global button_frame_y
 
-    if button_frame_y < 380:
+    if button_frame_y < 340:
         button_frame_y = button_frame_y + 5
         button_frame.place(relx=0.5,y=button_frame_y)
         app.after(16, animate_buttons)
     else:
         timer_label.place(
             relx=0.5,
-            rely=0.2,
+            rely=0.13,
             anchor="n"
         )
         title_label.tkraise()
-        title_label.configure(text="Focus Time", font=("SansSerifExbFLF", 100), text_color = "#CE2121")
-        title_label.pack_configure(pady=(30,20))
+        title_label.configure(text="Focus Time", font=("SansSerifExbFLF", 100), text_color = "#891212")
+        title_label.pack_configure(pady=(20,20))
         running= True
         timekeeper()
 # -----------------------------------------
@@ -114,7 +144,9 @@ def start_timer():
         start_button.pack_forget()
 
         pause_button.pack(side="left",padx=10, pady=10)
-        stop_button.pack(side="right", padx=10,pady=10)
+        stop_button.pack(side="left", padx=10, pady=10)
+        music_button.pack(side="left",padx=10, pady=10)
+        settings_button.pack(side="left", padx=10,pady=10)
 
         engine_sound.play()
         app.after(700, animate_car)
@@ -126,12 +158,12 @@ def pause_timer():
     paused = not paused
 
     if paused:
-        pause_button.configure(text="▶")
+        pause_button.configure(text="Continue")
         if timer_id != None:
             app.after_cancel(timer_id)
 
     else:
-        pause_button.configure(text="⏸")
+        pause_button.configure(text="Pause")
         timekeeper()
 
 def skip_timer():
@@ -150,15 +182,17 @@ def skip_timer():
     timekeeper()
 # -------------------------------------
 
+# menu --------------------------------
+
 
 # header -------------------------------
 title_label = ctk.CTkLabel(
     app,
     text="SimplyLovelyTimer",
-    font=("SansSerifBldFLF", 73),
+    font=("SansSerifBldFLF", 83),
     text_color="#FFFFFF"
 )
-title_label.pack(pady=(40,0))
+title_label.pack(pady=(20,0))
 
 car_label = ctk.CTkLabel(
     app,
@@ -204,31 +238,81 @@ start_button.pack(pady=10)
 
 pause_button = ctk.CTkButton(
     button_frame,
-    text="⏸",
+    text="Pause",
     command=pause_timer,
 
-    width=100,
-    height=60,
-    font=("Iosevka Charon Mono", 30),
-    text_color="#FFFFFF",
+    font=("SansSerifBldFLF", 24),
+    text_color="#891212",
 
-    fg_color="#891212",
-    hover_color="#531B1B",
-    corner_radius=30
+    width=30,
+    height= 80,
+    
+    border_width=2,
+    border_color="#891212",
+    corner_radius=30,
+
+    fg_color="transparent",
+    #fg_color="#891212",
+    #hover_color="#531B1B",
 )
 
 stop_button = ctk.CTkButton(
     button_frame,
-    text="✖",
+    text="Stop",
     command=skip_timer,
 
-    width=60,
-    height=60,
-    font=("Iosevka Charon Mono", 30),
-    text_color="#FFFFFF",
+    font=("SansSerifBldFLF", 24),
+    text_color="#891212",
 
-    fg_color="#891212",
-    hover_color="#531B1B",
+    width=30,
+    height= 80,
+
+    border_width=2,
+    border_color="#891212",
+
+    fg_color="transparent",
+    #fg_color="#891212",
+    #hover_color="#531B1B",
+    corner_radius=30
+)
+
+music_button = ctk.CTkButton(
+    button_frame,
+    text="Music",
+    command=skip_timer,
+
+    font=("SansSerifBldFLF", 24),
+    text_color="#891212",
+
+    width=30,
+    height= 80,
+
+    border_width=2,
+    border_color="#891212",
+
+    fg_color="transparent",
+    #fg_color="#891212",
+    #hover_color="#531B1B",
+    corner_radius=30
+)
+
+settings_button = ctk.CTkButton(
+    button_frame,
+    text="Settings",
+    command=skip_timer,
+
+    font=("SansSerifBldFLF", 24),
+    text_color="#891212",
+
+    width=30,
+    height= 80,
+
+    border_width=2,
+    border_color="#891212",
+
+    fg_color="transparent",
+    #fg_color="#891212",
+    #hover_color="#531B1B",
     corner_radius=30
 )
 #-------------------------------------------
